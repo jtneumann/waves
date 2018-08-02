992ae77cba9901524d2f613cda23fd25f3188e07
Jeff Neumann <jtneumann@hotmail.com>
2018-08-02 16:15:08 -0400

    SERVER setup complete thru 81: Brand model and routes

--- server/server.js
+++ server/server.js
@@ -18,6 +18,34 @@ app.use(cookieParser());
 // Models
 //---------
 const {User} = require('./models/user');
+const {Brand } = require('./models/brand');
+
+//Middlewares
+const { auth } = require('./middleware/auth');
+const { admin } = require('./middleware/admin');
+
+//=================
+// BRAND
+//=================
+
+app.post('/api/product/brand', auth,admin, (req,res)=> {
+    const brand = new Brand(req.body);
+
+    brand.save((err,doc)=>{
+        if(err) return res.json({success:false,err});
+        res.status(200).json({
+            success:true,
+            brand:doc
+        })
+    })
+})
+
+app.get('/api/product/brands', (req,res)=>{
+    Brand.find({},(err,brands)=>{
+        if(err) return res.status(400).send(err);
+        res.status(200).send(brands);
+    })
+})
 
 
 
@@ -25,8 +53,62 @@ const {User} = require('./models/user');
 // USERS
 //=================
 
+app.get('/api/users/auth',auth, (req,res)=>{
+    res.status(200).json({
+        isAdmin: req.user.role ===0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        cart: req.user.cart,
+        history: req.user.history
+    })
+});
+//REGISER ROUTE
 app.post('/api/users/register', (req, res) => {
-    res.status(200);
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+        if(err) return res.json({success: false, err});
+        res.status(200).json({
+            success: true,
+            // userdata: doc
+        });
+    });
+});
+//LOGIN ROUTE
+app.post('/api/users/login', (req, res) =>{
+    //find the email
+    User.findOne({'email': req.body.email}, (err, user)=> {
+        if(!user) return res.json({loginSuccess: false, message:'Auth failed, email not found'});
+        //check password
+        user.comparePassword(req.body.password, (err, isMatch)=> {
+            if(!isMatch) return res.json({loginSuccess:false, message: 'Incorrect Password'});
+                //generate token ?why not showing in robo3T
+            user.generateToken((err, user) =>{
+                if(err) return res.status(400).send(err);
+                res.cookie('w_auth',user.token).status(200).json({
+                    loginSuccess: true
+                });
+            });
+        });
+    
+    });
+});
+// LOGOUT ROUTE
+app.get('/api/users/logout',auth,(req,res)=> {
+    User.findByIdAndUpdate(
+        { _id:req.user._id },
+        { token: '' },
+        (err,doc)=>{
+            if(err) return res.json({success:false,err});
+            return res.status(200).send({
+                success:true
+            })
+        }
+
+    )
 });
 
 

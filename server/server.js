714f30ce5a88997371767428e201c4976ec4fc3c
Jeff Neumann <jtneumann@hotmail.com>
2018-08-03 13:09:22 -0400

    server side complete; next lec86

--- server/server.js
+++ server/server.js
@@ -30,6 +30,53 @@ const { admin } = require('./middleware/admin');
 // PRODUCTS
 //=================
 
+//BY ARRIVAL
+// /aricles?sortBy=createdAt&order=desc&limit=4
+app.get('/api/product/articles',(req,res)=>{
+
+    let order = req.query.order ? req.query.order : 'asc';
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+
+    Product.find().
+    populate('brand').
+    populate('wood').
+    sort([[sortBy,order]]).
+    limit(limit).
+    exec((err,articles)=>{
+        if(err) return res.status(400).send(err);
+        res.send(articles);
+    })
+
+});
+
+// BY SELL
+// /aricles?sortBy=sold&order=desc&limit=4
+
+
+//lec 84 all works accept the populate wood is returning null?corrected=now working by inserting //data into db thru robo3t
+// /api/product/article?id=JLKJLK,ASDFDFD,ASDFDFS&type=array or single
+app.get('/api/product/articles_by_id',(req,res)=>{
+    let type = req.query.type;
+    let items = req.query.id;
+
+    if(type === "array"){
+        let ids = req.query.id.split(',');
+        items = [];
+        items = ids.map(item=>{
+            return mongoose.Types.ObjectId(item);
+        })
+    }
+
+    Product.find({'_id':{$in:items}}).
+    populate('brand').populate('wood').
+    exec((err,docs)=>{
+        return res.status(200).send(docs);
+    })
+
+
+});
+
 app.post('/api/product/article',auth,admin, (req,res)=>{
     const product = new Product(req.body);
 

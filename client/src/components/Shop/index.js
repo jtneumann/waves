525de35f1f6ac535f1df4787c82fa54c7b454622
Jeff Neumann <jtneumann@hotmail.com>
2018-08-14 14:12:16 -0400

    107:CLIENT - Finished checkboxex on  page

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PageTop from '../utils/page_top';
 
+import { frets } from '../utils/Form/fixed_categories';
+
 import { connect } from 'react-redux';
 import { getBrands, getWoods } from '../../actions/products_actions';
 
@@ -8,14 +10,30 @@ import CollapseCheckbox from '../utils/collapseCheckbox';
 
 class Shop extends Component {
 
+    state = {
+        grid:'',
+        limit:6,
+        skip:0,
+        filters:{
+            brand:[],
+            frets:[],
+            wood:[],
+            price:[]
+        }
+    }
     
     componentDidMount() {
         this.props.dispatch(getBrands());
         this.props.dispatch(getWoods());
     }
     
-    handleFilters = () => {
-        
+    handleFilters = (filters,category) => {
+        const newFilters = {...this.state.filters}
+        newFilters[category] = filters;
+
+        this.setState({
+            filters: newFilters
+        })
     }
 
     render() {
@@ -33,6 +51,18 @@ class Shop extends Component {
                                 title="Brands"
                                 list={products.brands}
                                 handleFilters={(filters)=> this.handleFilters(filters,'brand')}
+                            />
+                            <CollapseCheckbox
+                                initState={false}
+                                title="Frets"
+                                list={frets}
+                                handleFilters={(filters)=> this.handleFilters(filters,'frets')}
+                            />
+                            <CollapseCheckbox
+                                initState={true}
+                                title="Woods"
+                                list={products.woods}
+                                handleFilters={(filters)=> this.handleFilters(filters,'wood')}
                             />
                         </div>
                         <div className="right">

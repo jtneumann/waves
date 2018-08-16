e7ed6cfa6b11d0b27f92b083a59e5b875eaf8cba
Jeff Neumann <jtneumann@hotmail.com>
2018-08-16 16:03:37 -0400

    115:ADMIN-add_product route started
    Info from the AddProduct component not showing yet
    
    Unable to locate the error - seems to be a vs code issue

--- client/src/components/utils/Form/formfield.js
+++ client/src/components/utils/Form/formfield.js
@@ -22,6 +22,11 @@ const Formfield = ({formdata,change,id}) => {
             case('input'):
                 formTemplate=(
                     <div className="formBlock">
+                        { formdata.showlabel ?
+                            <div className="label_inputs">
+                                {formdata.config.label}
+                            </div>
+                        :null}
                         <input
                             {...formdata.config}
                             value={formdata.value}

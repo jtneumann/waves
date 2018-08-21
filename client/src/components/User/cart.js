239edfc656439b7409d8c2dc1612b9d062d3bfff
Jeff Neumann <jtneumann@hotmail.com>
2018-08-21 14:27:58 -0400

    132:ADMIN_added method to calculate total

--- client/src/components/User/cart.js
+++ client/src/components/User/cart.js
@@ -29,12 +29,37 @@ class UserCart extends Component {
                     cartItems.push(item.id)
                 });
                 this.props.dispatch(getCartItems(cartItems,user.userData.cart))
+                    .then(()=>{
+                        if (this.props.user.cartDetail.length > 0) {
+                            this.calculateTotal(this.props.user.cartDetail);
+                        }
+                    })
             }
         }
 
     }
     
+    calculateTotal = (cartDetail) => {
+        let total = 0;
+        
+        cartDetail.forEach(item=>{
+            total += parseInt(item.price, 10) * item.quantity
+        });
 
+        this.setState({
+            total,
+            showTotal: true
+        });
+    }
+
+    showNoItemMessage = () => (
+        <div className="cart_no_items">
+            <FontAwesomeIcon icon={faFrown}/>
+            <div>
+                Cart is currently empty.
+            </div>
+        </div>
+    )
     render(){
         return (
             <UserLayout>
@@ -46,8 +71,37 @@ class UserCart extends Component {
                             type="cart"
                             removeItem={(id)=> this.removeFromCart(id)}
                         />
+                        { this.state.showTotal ?
+                            <div>
+                                <div className="user_cart_sum">
+                                    <div>
+                                        Total amount:  ${this.state.total}
+                                    </div>
 
+                                </div>
+                            </div>
+                        :
+                            this.state.showSuccess ?
+                                <div className="cart_success">
+                                    <FontAwesomeIcon icon={faSmile}/>
+                                    <div>
+                                        THANK YOU
+                                    </div>
+                                    <div>
+                                        Your order is now complete
+                                    </div>
+                                </div>
+                            :
+                            this.showNoItemMessage()
+                        }
                     </div>
+                    {
+                        this.state.showTotal ?
+                            <div className="paypal_button_container">
+                                Paypal
+                            </div>
+                        :null
+                    }
                 </div>
             </UserLayout>
         );
